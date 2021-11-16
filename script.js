265d7640d06e4ed4423efc4af15506d300969ff1
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2021-11-16 18:37:03 +0100

    create repo and law nots

---
+++ script.js
@@ -0,0 +1,9 @@
+const sectionTitles = document.querySelectorAll('.section-title'),
+    textContainers = document.querySelectorAll('.text-container');
+
+
+for (const sectionTitle of sectionTitles) {
+    sectionTitle.addEventListener('click', function () {
+
+    });
+}
