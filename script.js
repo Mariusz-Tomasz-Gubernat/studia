7e5f8969094a6602b2cb3a79c04bdbd4437e261f
Mariusz Gubernat <mariusz.gubernat@protonmail.com>
2021-12-14 20:29:53 +0100

    adds some fixes

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const sectionTitles = document.querySelectorAll('.section-title'),
-    textContainers = document.querySelectorAll('.text-container');
+const sectionTitles = document.querySelectorAll(".container .section-title"),
+    textContainers = document.querySelectorAll(".container .text-container"),
+    selectCards = document.querySelectorAll(".select-title"),
+    selectCardContents = document.querySelectorAll(".select-card-content");
 
+for (let selectCard of selectCards) {
+    selectCard.addEventListener('click', function () {
+        const selectCardID = selectCard.getAttribute('id');
+        for (let selectCardContent of selectCardContents) {
+            const selectCardContentID = selectCardContent.getAttribute('id').replace("#", "");
+            if (selectCardID === selectCardContentID) {
+                selectCardContent.classList.toggle('active');
+            } else {
+                selectCardContent.classList.remove('active');
+            }
+        }
+    })
+}
 
-for (const sectionTitle of sectionTitles) {
+for (let sectionTitle of sectionTitles) {
     sectionTitle.addEventListener('click', function () {
 
-    });
+        const sectionTitleID = sectionTitle.getAttribute('id');
+
+        for (let textContainer of textContainers) {
+            const textContainerID = textContainer.getAttribute('id').replace("#", "");
+
+            if (sectionTitleID === textContainerID) {
+                textContainer.classList.toggle('active');
+            } else {
+                textContainer.classList.remove('active');
+            }
+        }
+    })
 }
